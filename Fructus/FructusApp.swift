58901d222ef97643e87c60e2997198b7f8a7bd4a
Yusuf Çağlar <faf7faf7@gmail.com>
2021-03-25 19:36:52 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Yusuf ÇAĞLAR on 25/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
